37979ee53f32d0cc4e261a78de529fa66abdee36
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2026-07-29 19:35:01 -0400

    Arreglando versiones

--- app/src/main/java/com/example/enmanuel_gomez_ap2_p2/di/AppModule.kt
+++ app/src/main/java/com/example/enmanuel_gomez_ap2_p2/di/AppModule.kt
@@ -1,14 +1,15 @@
 package com.example.enmanuel_gomez_ap2_p2.di
 
-import com.example.enmanuel_gomez_ap2_p2.data.remote.api.EntidadApi
-import com.example.enmanuel_gomez_ap2_p2.data.repository.EntidadRepositoryImpl
-import com.example.enmanuel_gomez_ap2_p2.domain.repository.EntidadRepository
+import com.example.enmanuel_gomez_ap2_p2.data.remote.api.GastoApi
+import com.example.enmanuel_gomez_ap2_p2.data.repository.GastoRepositoryImpl
+import com.example.enmanuel_gomez_ap2_p2.domain.repository.GastoRepository
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,22 +18,20 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("https://api.example.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+    fun provideMoshi(): Moshi = Moshi.Builder().build()
 
     @Provides
     @Singleton
-    fun provideEntidadApi(retrofit: Retrofit): EntidadApi {
-        return retrofit.create(EntidadApi::class.java)
-    }
+    fun provideRetrofit(moshi: Moshi): Retrofit = Retrofit.Builder()
+        .baseUrl("https://api-2026-h7eddqgydxc0fmau.eastus2-01.azurewebsites.net/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
 
     @Provides
     @Singleton
-    fun provideEntidadRepository(api: EntidadApi): EntidadRepository {
-        return EntidadRepositoryImpl(api)
-    }
+    fun provideGastoApi(retrofit: Retrofit): GastoApi = retrofit.create(GastoApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideGastoRepository(api: GastoApi): GastoRepository = GastoRepositoryImpl(api)
 }
